d78606f691c64d5ee494c33619798b202ed6d42f
Cannafox <deuterpl@gmail.com>
2025-01-15 06:17:58 +0100

    change theme

--- .config/fish/conf.d/00_env_variables.fish
+++ .config/fish/conf.d/00_env_variables.fish
@@ -13,6 +13,7 @@ set -gx VK_LAYER_PATH $VULKAN_SDK/share/vulkan/explicit_layer.d
 set -gx VK_ADD_LAYER_PATH $VULKAN_SDK/share/vulkan/explicit_layer.d
 set -gx VK_LOADER_LAYERS_ENABLE "*api_dump:*validation"
 set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx FZF_DEFAULT_OPTS "--border --height 40% --layout=reverse --color=bg+:#141a1f --color=fg:#b3c1cc --color=fg+:#b3c1cc --color=hl:#52697a --color=hl+:#52697a --color=border:#3d4f5c --color=info:#75bdf0 --color=marker:#a875f0 --color=pointer:#ff007b --color=prompt:#ff007b --color=spinner:#ff007b --color=header:#757ff0"
 
 
 # fish
